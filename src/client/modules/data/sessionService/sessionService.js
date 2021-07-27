67e2ccb7b7778ef7a83175445cedc8cea568d7b9
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-07-27 10:09:34 +0530

    added new express api url, file format

--- src/client/modules/data/sessionService/sessionService.js
+++ src/client/modules/data/sessionService/sessionService.js
@@ -3,22 +3,20 @@ const URL = 'https://salesforce-conference-api.herokuapp.com/api/sessions';
 let sessions = [];
 
 export const getSessions = () => fetch(URL)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('No Response from Server');
-    }
-    return response.json();
-})
-.then(result => {
-    sessions = result.data;
-    return sessions;
-});
-console.log('sessions::',sessions);
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('No Response from Server');
+        }
+        return response.json();
+    })
+    .then(result => {
+        sessions = result.data;
+        return sessions;
+    });
+console.log('sessions::', sessions);
 
 export const getSession = sessionId => {
     return sessions.find(session => {
         return session.id === sessionId;
     });
 }
-
-
